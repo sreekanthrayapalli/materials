9c1260db428e743f0f487d141af0c68093af2234
srikanth <sreekanth.rayapalli@gmail.com>
2020-06-13 08:00:07 +0530

    uploaded

---
+++ springbatch/src/main/java/com/example/demo/batchsteps/StringReader.java
@@ -0,0 +1,26 @@
+package com.example.demo.batchsteps;
+
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+
+public class StringReader implements ItemReader<String>{
+	
+	String []arr = {"abc","xyz","pqr"};
+
+	int a = -1;
+	@Override
+	public String read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
+		// TODO Auto-generated method stub
+		if(arr.length < a) {
+			a++;
+			return arr[a];
+		}
+		else {
+			a= -1;
+		}
+		return null;
+	}
+
+}
